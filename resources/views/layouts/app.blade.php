443b19abf077681020cfaf0ed9e836c8cbee418d
Rob de Willigen <robdewilligen@outlook.com>
2021-09-30 14:01:58 +0200

    Add front-end
    Add some front end to first pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,8 +9,34 @@
 
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
-    <body class="bg-gray-100">
-            <nav class="p-6 bg-white flex justify-between">
+    <body class="bg-gray-200">
+            <nav class="p-6 bg-white flex justify-between mb-6">
+                <ul class="flex item-center">
+                    <li>
+                        <a href="#" class="p-3">Home</a>
+                    </li>
+                    <li>
+                        <a href="#" class="p-3">Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="#" class="p-3">Post</a>
+                    </li>
+                </ul>
+
+                <ul class="flex item-center">
+                    <li>
+                        <a href="#" class="p-3">John Smith</a>
+                    </li>
+                    <li>
+                        <a href="#" class="p-3">Login</a>
+                    </li>
+                    <li>
+                        <a href="#" class="p-3">Register</a>
+                    </li>
+                    <li>
+                        <a href="#" class="p-3">Logout</a>
+                    </li>
+                </ul>
 
             </nav>
         @yield('content')
