6200796ad243bb668f2e8d8cc7aa2fdbdaa8a49e
Rob de Willigen <robdewilligen@outlook.com>
2021-10-25 19:05:30 +0200

    Add Authorisation to deleting, PostPolicy,

---
+++ resources/views/users/posts/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex justify-center">
+        <div class="w-8/12 bg-white p-6 rounded-lg">
+
+            @if($posts->count())
+                @foreach($posts as $post)
+                    <x-post :post="$post"/>
+                @endforeach
+
+                {{ $posts->links() }}
+            @else
+                <p>There are no posts available!</p>
+            @endif
+
+        </div>
+    </div>
+@endsection
